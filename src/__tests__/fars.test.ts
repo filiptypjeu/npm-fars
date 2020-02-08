c8a53a6aa5d078fb3a20b06745472ebdcc95a440
Filip Stenbacka <filip.stenbacka@trimble.com>
2020-02-09 00:48:54 +0200

    Modifed the test variables a bit. Also format.

--- src/__tests__/fars.test.ts
+++ src/__tests__/fars.test.ts
@@ -1,5 +1,5 @@
-import { bookings, setFarsURL, IFarsBooking } from '../index';
-import { myBaseURL, myBookables, myTestparameters } from './variables/fars.test_variables';
+import { bookings, setFarsParams, IFarsBooking } from "../index";
+import { myBaseURL, myUsername, myPassword, myTestparameters, myBookables } from "./variables/fars.test_variables";
 
 const testObject = (b: IFarsBooking) => {
   expect(b.id).toBeDefined();
@@ -15,45 +15,61 @@ const testObject = (b: IFarsBooking) => {
   expect(b.user.username).toBeDefined();
 };
 
-test('bookings nourl', async () => {
+test("bookings nourl", async () => {
   await expect(bookings()).rejects.toThrowError();
-}, 10000);
+}, 5000);
 
-test('setFarsURL', async () => {
-  expect(setFarsURL(myBaseURL)).toEqual(myBaseURL);
-}, 10000);
+test("setBadLoginPath", async () => {
+  expect(setFarsParams(myBaseURL, myUsername, myPassword, "notcorrect")).toEqual(undefined);
+}, 5000);
 
-test('bookings', async () => {
+test("bookings bad login path", async () => {
+  await expect(bookings()).rejects.toThrowError();
+}, 5000);
+
+test("setBadApiPath", async () => {
+  expect(setFarsParams(myBaseURL, myUsername, myPassword, undefined, "notCorrect")).toEqual(undefined);
+}, 5000);
+
+test("bookings bad api path", async () => {
+  await expect(bookings()).rejects.toThrowError();
+}, 5000);
+
+test("setFarsParams", async () => {
+  expect(setFarsParams(myBaseURL, myUsername, myPassword)).toEqual(undefined);
+}, 5000);
+
+test("bookings", async () => {
   const a = await bookings();
   testObject(a.result[0]);
 }, 10000);
 
-test('bookings start', async () => {
-  const a = await bookings(new Date('2019-10-01'));
+test("bookings start", async () => {
+  const a = await bookings(new Date("2019-10-01"));
   testObject(a.result[0]);
 }, 10000);
 
-test('bookings end', async () => {
-  const a = await bookings(undefined, new Date('2019-10-01'));
+test("bookings end", async () => {
+  const a = await bookings(undefined, new Date("2019-10-01"));
   testObject(a.result[0]);
 }, 10000);
 
-test('bookings bookables', async () => {
+test("bookings bookables", async () => {
   for (let bookable of myBookables) {
     const a = await bookings(undefined, undefined, bookable);
     testObject(a.result[0]);
   }
 }, 10000);
 
-test('bookings allparameters', async () => {
+test("bookings allparameters", async () => {
   for (let bookable of myBookables) {
-    const a = await bookings(new Date('2019-01-01'), new Date('2019-12-31'), bookable);
+    const a = await bookings(new Date("2019-01-01"), new Date("2019-12-31"), bookable);
 
     testObject(a.result[0]);
   }
 }, 10000);
 
-test('bookings specific', async () => {
+test("bookings specific", async () => {
   const a = await bookings(myTestparameters.dateFrom, myTestparameters.dateTo, myTestparameters.bookable);
   expect(a.result).toHaveLength(1);
   expect(a.result[0]).toEqual(myTestparameters.result);
--- src/__tests__/variables/fars.test_variables_template.ts
+++ src/__tests__/variables/fars.test_variables_template.ts
@@ -2,13 +2,15 @@
  * URL for your FARS API.
  * String.
  */
-export const myBaseURL = '';
+export const myBaseURL = "";
+export const myUsername = "";
+export const myPassword = "";
 
 /**
  * Your FARS bookables.
  * Array of strings.
  */
-export const myBookables = ['myBookable1', 'myBookable2'];
+export const myBookables = ["myBookable1", "myBookable2"];
 
 /**
  * Your test one test case.
@@ -18,20 +20,20 @@ export const myTestparameters = {
   result: {
     id: 0,
     user: {
-      username: 'username',
-      first_name: 'first',
-      last_name: 'last',
+      username: "username",
+      first_name: "first",
+      last_name: "last",
     },
     booking_group: {
-      name: 'group',
+      name: "group",
     },
-    start: '1970-01-01T01:00:00+02:00',
-    end: '1970-01-01T02:00:00+02:00',
-    comment: 'comment',
+    start: "1970-01-01T01:00:00+02:00",
+    end: "1970-01-01T02:00:00+02:00",
+    comment: "comment",
     bookable: 1,
     repeatgroup: null,
   },
-  dateFrom: new Date('1970-01-01T00:00:00+02:00'),
-  dateTo: new Date('1970-01-02T00:00:00+02:00'),
-  bookable: 'myBookable1',
+  dateFrom: new Date("1970-01-01T00:00:00+02:00"),
+  dateTo: new Date("1970-01-02T00:00:00+02:00"),
+  bookable: "myBookable1",
 };
