1023b26bd1a8587893b0c3f31dfccb3b37bf04b5
Filip Stenbacka <filip.stenbacka@gmail.com>
2021-05-28 01:35:54 +0300

    Added the correct test variable template.

--- src/__tests__/variables/fars.test_variables_template.ts
+++ src/__tests__/variables/fars.test_variables_template.ts
@@ -1,40 +1,47 @@
 /**
- * URL for your FARS API.
- * String.
+ * Variables
  */
 export const myBaseURL = "";
 export const myUsername = "";
 export const myPassword = "";
 
 /**
- * Your FARS bookables.
- * Array of strings.
+ * Bookables
  */
-export const myBookables = ["myBookable1", "myBookable2"];
+export const myBookables = ["myBookable1", "myBookable2", "myBookable3"];
 
 /**
- * Your test one test case.
- * Object containging search parameters and result.
+ * Test broad cases, where the fetches gives at least 2 bookings.
  */
-export const myTestparameters = {
+export const myBroadCases = {
+  runThisTest: false,
+  dateFrom: new Date("2020-01-01T00:00:00+02:00"),
+  dateTo: new Date("2021-01-01T00:00:00+02:00"),
+  bookable: "myBookable1",
+};
+
+/**
+ * Test specific case, where the fetch only gets one result.
+ */
+export const mySpecificCase = {
+  runThisTest: false,
+  parameters: {
+    dateFrom: new Date("2020-01-01T00:00:00+02:00"),
+    dateTo: new Date("2020-01-02T00:00:00+02:00"),
+    bookable: "myBookable1",
+  },
   result: {
-    id: 0,
+    id: 42,
     user: {
-      username: "username",
-      first_name: "first",
-      last_name: "last",
+      username: "",
+      first_name: "",
+      last_name: "",
     },
-    booking_group: {
-      name: "group",
-    },
-    start: "1970-01-01T01:00:00+02:00",
-    end: "1970-01-01T02:00:00+02:00",
-    comment: "comment",
+    booking_group: null,
+    start: "2020-01-01T00:00:00+02:00",
+    end: "2020-01-02T00:00:00+02:00",
+    comment: "",
     bookable: 1,
     repeatgroup: null,
   },
-  dateFrom: new Date("1970-01-01T00:00:00+02:00"),
-  dateTo: new Date("1970-01-02T00:00:00+02:00"),
-  bookable: "myBookable1",
-  runThisTest: true,
 };
