239305d4be93770fff1a4845d7534913b5b5506e
Filip Stenbacka <filip.stenbacka@gmail.com>
2021-05-28 01:32:25 +0300

    Added more tests.

--- src/__tests__/fars.test.ts
+++ src/__tests__/fars.test.ts
@@ -15,6 +15,32 @@ const testObject = (b: IFarsBooking) => {
   expect(b.user.username).toBeDefined();
 };
 
+const testDateToday = (expected: Date, provided: Date) => {
+  expect(expected.getFullYear()).toEqual(provided.getFullYear());
+  expect(expected.getMonth()).toEqual(provided.getMonth());
+  expect(expected.getDate()).toEqual(provided.getDate());
+}
+
+const testDateNow = (expected: Date, provided: Date) => {
+  testDateToday(expected, provided);
+  expect(expected.getHours()).toEqual(provided.getHours());
+  expect(expected.getMinutes()).toEqual(provided.getMinutes());
+}
+
+const testDateMidnight = (provided: Date) => {
+  expect(provided.getHours()).toEqual(0);
+  expect(provided.getMinutes()).toEqual(0);
+  expect(provided.getSeconds()).toEqual(0);
+  expect(provided.getMilliseconds()).toEqual(0);
+}
+
+const testDateAlmostMidnight = (provided: Date) => {
+  expect(provided.getHours()).toEqual(23);
+  expect(provided.getMinutes()).toEqual(59);
+  expect(provided.getSeconds()).toEqual(59);
+  expect(provided.getMilliseconds()).toEqual(999);
+}
+
 const fars = new FARSManager(myBaseURL, myUsername, myPassword);
 
 test("bookings nourl", async () => {
@@ -25,16 +51,25 @@ if (myBroadCases.runThisTest) {
   test("bookings start", async () => {
     const a = await fars.bookings(myBroadCases.dateFrom, undefined, myBroadCases.bookable);
     testObject(a.result[0]);
+    expect(a.bookable).toEqual(myBroadCases.bookable);
+    expect(a.start).toEqual(myBroadCases.dateFrom);
+    expect(a.end).toEqual(undefined);
   }, 10000);
 
   test("bookings end", async () => {
     const a = await fars.bookings(undefined, myBroadCases.dateTo, myBroadCases.bookable);
     testObject(a.result[0]);
+    expect(a.bookable).toEqual(myBroadCases.bookable);
+    expect(a.start).toEqual(undefined);
+    expect(a.end).toEqual(myBroadCases.dateTo);
   }, 10000);
 
   test("bookings start and end", async () => {
     const a = await fars.bookings(myBroadCases.dateFrom, myBroadCases.dateTo, myBroadCases.bookable);
     testObject(a.result[0]);
+    expect(a.bookable).toEqual(myBroadCases.bookable);
+    expect(a.start).toEqual(myBroadCases.dateFrom);
+    expect(a.end).toEqual(myBroadCases.dateTo);
   }, 10000);
 }
 
@@ -42,7 +77,96 @@ test("bookings bookables", async () => {
   for (let bookable of myBookables) {
     const a = await fars.bookings(undefined, undefined, bookable);
     testObject(a.result[0]);
+    expect(a.bookable).toEqual(bookable);
+    expect(a.start).toEqual(undefined);
+    expect(a.end).toEqual(undefined);
+    expect(a.url).toEqual(`${myBaseURL}/api/bookings?bookable=${bookable}&after=&before=&format=json`);
   }
+}, 10000);
+
+test("bookings from now positive", async () => {
+  const d = new Date();
+  const a = await fars.bookingsFromNow(10, myBookables[0]);
+
+  expect(a.bookable).toEqual(myBookables[0]);
+  expect(a.start).toBeDefined();
+  expect(a.end).toBeDefined();
+
+  testDateNow(a.start!, d);
+  d.setDate(d.getDate() + 10);
+  testDateNow(a.end!, d);
+}, 10000);
+
+test("bookings from now negative", async () => {
+  const d = new Date();
+  const a = await fars.bookingsFromNow(-10, myBookables[0]);
+
+  expect(a.bookable).toEqual(myBookables[0]);
+  expect(a.start).toBeDefined();
+  expect(a.end).toBeDefined();
+
+  testDateNow(a.end!, d);
+  d.setDate(d.getDate() - 10);
+  testDateNow(a.start!, d);
+}, 10000);
+
+test("bookings from now zero", async () => {
+  const d = new Date();
+  const a = await fars.bookingsFromNow(0, myBookables[0]);
+
+  expect(a.bookable).toEqual(myBookables[0]);
+  expect(a.start).toBeDefined();
+  expect(a.end).toBeDefined();
+
+  testDateNow(a.start!, d);
+  expect(a.start).toEqual(a.end);
+}, 10000);
+
+test("bookings from today positive", async () => {
+  const d = new Date();
+  const a = await fars.bookingsFromToday(10, myBookables[0]);
+
+  expect(a.bookable).toEqual(myBookables[0]);
+  expect(a.start).toBeDefined();
+  expect(a.end).toBeDefined();
+
+  testDateMidnight(a.start!);
+  testDateAlmostMidnight(a.end!);
+
+  testDateToday(a.start!, d);
+  d.setDate(d.getDate() + 10);
+  testDateToday(a.end!, d);
+}, 10000);
+
+test("bookings from today negative", async () => {
+  const d = new Date();
+  const a = await fars.bookingsFromToday(-10, myBookables[0]);
+
+  expect(a.bookable).toEqual(myBookables[0]);
+  expect(a.start).toBeDefined();
+  expect(a.end).toBeDefined();
+
+  testDateMidnight(a.start!);
+  testDateAlmostMidnight(a.end!);
+
+  testDateToday(a.end!, d);
+  d.setDate(d.getDate() - 10);
+  testDateToday(a.start!, d);
+}, 10000);
+
+test("bookings from today zero", async () => {
+  const d = new Date();
+  const a = await fars.bookingsFromToday(0, myBookables[0]);
+
+  expect(a.bookable).toEqual(myBookables[0]);
+  expect(a.start).toBeDefined();
+  expect(a.end).toBeDefined();
+
+  testDateMidnight(a.start!);
+  testDateAlmostMidnight(a.end!);
+
+  testDateToday(a.start!, d);
+  testDateToday(a.end!, d);
 }, 10000);
 
 if (mySpecificCase.runThisTest) {
